8e4de5626da2c72de7fe0a186c2de8d5e5f83d20
LavakauT <132649549+LavakauT@users.noreply.github.com>
2023-11-06 17:34:20 +0800

    Add files via upload

---
+++ RF.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+date
+echo "job submitted"
+echo "requiring 20 tasks under this lovely user"
+
+#SBATCH -A acl81744
+#SBATCH -n 20
+#SBATCH --mem 20G
+#SBATCH --t 00-12:00
+
+echo "Environment loading....."
+
+cd /RAID1/working/R425/lavakau/ML_master
+echo "python 3.7.0 envs activate: ml"
+source /RAID1/working/R425/lavakau/miniconda3/etc/profile.d/conda.sh
+conda activate ml
+
+
+
+for inp in net/UU/*df_p0.01_mod.txt; do echo "python ML_classification_modified.py -df $inp  -test $inp.test -cl_train pos,neg -alg RF -apply all -plots T"; done
+
+for inp in net/UU/*df_p0.01_mod.txt; do python ML_classification_modified.py -df $inp  -test $inp.test -cl_train pos,neg -alg RF -apply all -plots T; done
+
+echo "job finished"
+
+
+
