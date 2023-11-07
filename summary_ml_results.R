ef60802bc0e8e68de77b101aedd6f8eda10c992b
LavakauT <132649549+LavakauT@users.noreply.github.com>
2023-11-07 10:40:13 +0800

    Add files via upload

--- summary_ml_results.R
+++ summary_ml_results.R
@@ -42,44 +42,95 @@ for (z in 1:3) {
 library(tidyverse)
 # library(pgirmess)
 library(ggplot2)
+
 dir <- '/Volumes/R425/RNA-seq/arabidopsis/training/tss_5utr_promoter_pcc/'
-sub.dir <- 'DD/'
+group <- c('DD', 'UU')
 treatment <- 'HS'
-alg1 <- 'SVM' # change algorithm SVM/RF/LogReg
+alg1 <- 'SVM' # different algorithm SVM/RF/LogReg
 alg2 <- 'RF'
 alg3 <- 'LogReg'
 
+for (x in group) {
+  if(x == 'DD'){
+    sub.dir <- paste0(x, '/')
+    
+    df.svm <- read.delim(paste0(dir, sub.dir, paste(treatment, alg1, sep = '_'), '_sum2.txt'))
+    df.rf <- read.delim(paste0(dir, sub.dir, paste(treatment, alg2, sep = '_'), '_sum2.txt'))
+    df.logreg <- read.delim(paste0(dir, sub.dir, paste(treatment, alg3, sep = '_'), '_sum2.txt'))
+    
+    df.svm <- df.svm[c(2,14),]# AUC-ROC and F1
+    row.names(df.svm) <- c('AUC', 'F1')
+    df.svm <- t(df.svm)
+    
+    df.rf <- df.rf[c(2,14),]
+    row.names(df.rf) <- c('AUC', 'F1')
+    df.rf <- t(df.rf)
+    
+    df.logreg <- df.logreg[c(2,14),]
+    row.names(df.logreg) <- c('AUC', 'F1')
+    df.logreg <- t(df.logreg)
+    
+    df <- data.frame(rbind(df.svm, df.rf, df.logreg))
+    df <- rownames_to_column(df, var = 'algorithm')
+    df <- separate(df,algorithm,c('algorithm', 'times'))
+    
+    
+    df$group <- x
+    df.com <- df
+  } else{
+    df.svm <- read.delim(paste0(dir, sub.dir, paste(treatment, alg1, sep = '_'), '_sum2.txt'))
+    df.rf <- read.delim(paste0(dir, sub.dir, paste(treatment, alg2, sep = '_'), '_sum2.txt'))
+    df.logreg <- read.delim(paste0(dir, sub.dir, paste(treatment, alg3, sep = '_'), '_sum2.txt'))
+    
+    df.svm <- df.svm[c(2,14),]# AUC-ROC and F1
+    row.names(df.svm) <- c('AUC', 'F1')
+    df.svm <- t(df.svm)
+    
+    df.rf <- df.rf[c(2,14),]
+    row.names(df.rf) <- c('AUC', 'F1')
+    df.rf <- t(df.rf)
+    
+    df.logreg <- df.logreg[c(2,14),]
+    row.names(df.logreg) <- c('AUC', 'F1')
+    df.logreg <- t(df.logreg)
+    
+    df <- data.frame(rbind(df.svm, df.rf, df.logreg))
+    df <- rownames_to_column(df, var = 'algorithm')
+    df <- separate(df,algorithm,c('algorithm', 'times'))
+    
+    
+    df$group <- x
+    df.com <- rbind(df.com, df)
+  }
+  
+}
 
-df.svm <- read.delim(paste0(dir, sub.dir, paste(treatment, alg1, sep = '_'), '_sum2.txt'))
-df.rf <- read.delim(paste0(dir, sub.dir, paste(treatment, alg2, sep = '_'), '_sum2.txt'))
-df.logreg <- read.delim(paste0(dir, sub.dir, paste(treatment, alg3, sep = '_'), '_sum2.txt'))
 
-df.svm <- df.svm[c(2,14),]# AUC-ROC and F1
-row.names(df.svm) <- c('AUC', 'F1')
-df.svm <- t(df.svm)
+# F1 plot
+p <- ggplot(data = df.com, 
+            aes(x = group,
+                y = as.numeric(F1),
+                fill = as.factor(algorithm))) +
+  geom_boxplot() +
+  ylim(0.5, 1.0) +
+  geom_jitter(shape=16, position=position_dodge(1)) +
+  geom_hline(yintercept = 0.5, linetype = 'dashed') +
+  scale_fill_discrete(name = "Algorithm") +
+  scale_fill_brewer(palette="RdBu") +
+  labs(title = expression(bolditalic('pCRE ML results')), y = 'F1', fill = 'Algorithm') + # change y axis F1/AUC
+  coord_flip() +
+  theme_minimal() +
+  theme(axis.text.y = element_text(size = 20),
+        axis.title.y = element_blank(),
+        panel.grid.minor = element_blank())
 
-df.rf <- df.rf[c(2,14),]
-row.names(df.rf) <- c('AUC', 'F1')
-df.rf <- t(df.rf)
-
-df.logreg <- df.logreg[c(2,14),]
-row.names(df.logreg) <- c('AUC', 'F1')
-df.logreg <- t(df.logreg)
-
-df <- data.frame(rbind(df.svm, df.rf, df.logreg))
-df <- rownames_to_column(df, var = 'algorithm')
-df <- separate(df,algorithm,c('algorithm', 'times'))
-
-
-df$group <- 'DD'
-
-
-# this line only for first time
-# only for tak_hs all sites
-# df.com <- df
-df.com <- rbind(df.com, df)
+pdf(paste0(dir, 'F1.pdf'),
+    width = 4, height = 4)
+p
+dev.off()
 
 
+# AUC_ROC plot
 p <- ggplot(data = df.com, 
             aes(x = group,
                 y = as.numeric(AUC),
@@ -90,12 +141,15 @@ p <- ggplot(data = df.com,
   geom_hline(yintercept = 0.5, linetype = 'dashed') +
   scale_fill_discrete(name = "Algorithm") +
   scale_fill_brewer(palette="RdBu") +
-  labs(title = 'pCRE',x = 'groups of DEGs', y = 'AUC_ROC', fill = 'Algorithm') + # change y axis F1/AUC
+  labs(title = expression(bolditalic('pCRE ML results')), y = 'AUC-ROC', fill = 'Algorithm') + # change y axis F1/AUC
   coord_flip() +
-  theme(axis.text.x = element_text(size = 15)) +
-  theme_minimal()
+  theme_minimal() +
+  theme(axis.text.y = element_text(size = 20),
+        axis.title.y = element_blank(),
+        panel.grid.minor = element_blank())
 
-tiff(paste0(dir, 'AUC_ROC.tif'),
-     width = 1200, height = 700, res = 300)
+pdf(paste0(dir, 'AUC_ROC.pdf'),
+    width = 4, height = 4)
 p
 dev.off()
+
