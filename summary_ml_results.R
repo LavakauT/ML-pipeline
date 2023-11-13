1727bd1d4c1711c21829d7a301e026216ca53675
LavakauT <132649549+LavakauT@users.noreply.github.com>
2023-11-13 14:18:43 +0800

    Update summary_ml_results.R

--- summary_ml_results.R
+++ summary_ml_results.R
@@ -1,136 +1,98 @@
 # loop code for extracting ML resutls--------
 library(dplyr)
-# library(pgirmess)
+library(pgirmess)
+dir <- '/path/to/all/training/files'
+sub.dirs <- list.dirs(dir,
+                      full.names = FALSE,
+                      recursive = FALSE)
 
-dir <- '/Volumes/R425/RNA-seq/arabidopsis/training/tss_5utr_promoter_pcc/'
-sub.dir <- 'DD/'
-treatment <- 'HS'
 middle <- 'neg'
-algorithm <- c('SVM', 'RF', 'LogReg')
-ends <- c(paste0(rep('.fa.random_df_p0.01_mod.txt_', 3),
-                 algorithm, rep('_results.txt', 3)))
+ends <- paste('.fa.random_df_p0.01_mod.txt', # change the correct name which you assigned
+              c('SVM','RF','LogReg'),
+              'results.txt', sep = '_')
+algorithms <- c('SVM', 'RF', 'LogReg')
 
-for (z in 1:3) {
-  sub.algorithm <- algorithm[z]
-  sub.ends <- ends[z]
-  for ( i in 1:10){
-    if (i == 1){
-      number <- i
-      df <- read.delim(paste0(dir, sub.dir, paste(middle, number, sep = '_'), sub.ends))
-      df <- data.frame(df[18:65,]) # AUC-ROC to FN
-      colnames(df) <- paste(sub.algorithm, i, sep = '_')
-    } else{
-      number2 <- i
-      sub.df <- read.delim(paste0(dir, sub.dir, paste(middle, number2, sep = '_'), sub.ends))
-      sub.df <- data.frame(sub.df[18:65,])
-      colnames(sub.df) <- paste(sub.algorithm, i, sep = '_')
-      df <- cbind(df,sub.df)
+
+for (z in 1:length(sub.dirs)) {
+  sub.dir <- sub.dirs[z]
+  
+  for (x in 1:3) {
+    end <- ends[x]
+    algorithm <- algorithms[x]
+    
+    for ( i in 1:10){
+      if (i == 1){
+        number <- i
+        df <- read.delim(paste0(dir, '/', sub.dir, '/', paste(middle, number, sep = '_'), end))
+        df <- data.frame(df[18:65,]) # AUC-ROC to FN
+        colnames(df) <- paste(algorithm, i, sep = '_')
+      } else{
+        number2 <- i
+        sub.df <- read.delim(paste0(dir, '/', sub.dir, '/', paste(middle, number2, sep = '_'), end))
+        sub.df <- data.frame(sub.df[18:65,])
+        colnames(sub.df) <- paste(algorithm, i, sep = '_')
+        df <- cbind(df,sub.df)}
+      
+      # output result
+      write.delim(df, paste0(dir, '/', sub.dir, '/', paste(sub.dir, algorithm, sep = '_'), '_sum.txt'))   
     }
-    # output result
-    write.table(df,
-                paste0(dir, sub.dir, paste(treatment, sub.algorithm, sep = '_'), '_sum2.txt'),
-                row.names = F,
-                quote = F,
-                sep = '\t')
   }
 }
-  
-
 
 
 # code for summarize results--------
 library(tidyverse)
-# library(pgirmess)
+library(pgirmess)
 library(ggplot2)
+dir <- '/path/to/all/training/files'
+sub.dirs <- list.dirs(dir,
+                      full.names = FALSE,
+                      recursive = FALSE)
 
-dir <- '/Volumes/R425/RNA-seq/arabidopsis/training/tss_5utr_promoter_pcc/'
-group <- c('DD', 'UU')
-treatment <- 'HS'
-alg1 <- 'SVM' # different algorithm SVM/RF/LogReg
+middle <- 'neg'
+alg1 <- 'SVM' # change algorithm SVM/RF/LogReg
 alg2 <- 'RF'
 alg3 <- 'LogReg'
 
-for (x in group) {
-  if(x == 'DD'){
-    sub.dir <- paste0(x, '/')
-    
-    df.svm <- read.delim(paste0(dir, sub.dir, paste(treatment, alg1, sep = '_'), '_sum2.txt'))
-    df.rf <- read.delim(paste0(dir, sub.dir, paste(treatment, alg2, sep = '_'), '_sum2.txt'))
-    df.logreg <- read.delim(paste0(dir, sub.dir, paste(treatment, alg3, sep = '_'), '_sum2.txt'))
-    
-    df.svm <- df.svm[c(2,14),]# AUC-ROC and F1
-    row.names(df.svm) <- c('AUC', 'F1')
-    df.svm <- t(df.svm)
-    
-    df.rf <- df.rf[c(2,14),]
-    row.names(df.rf) <- c('AUC', 'F1')
-    df.rf <- t(df.rf)
-    
-    df.logreg <- df.logreg[c(2,14),]
-    row.names(df.logreg) <- c('AUC', 'F1')
-    df.logreg <- t(df.logreg)
-    
-    df <- data.frame(rbind(df.svm, df.rf, df.logreg))
-    df <- rownames_to_column(df, var = 'algorithm')
-    df <- separate(df,algorithm,c('algorithm', 'times'))
-    
-    
-    df$group <- x
-    df.com <- df
-  } else{
-    df.svm <- read.delim(paste0(dir, sub.dir, paste(treatment, alg1, sep = '_'), '_sum2.txt'))
-    df.rf <- read.delim(paste0(dir, sub.dir, paste(treatment, alg2, sep = '_'), '_sum2.txt'))
-    df.logreg <- read.delim(paste0(dir, sub.dir, paste(treatment, alg3, sep = '_'), '_sum2.txt'))
-    
-    df.svm <- df.svm[c(2,14),]# AUC-ROC and F1
-    row.names(df.svm) <- c('AUC', 'F1')
-    df.svm <- t(df.svm)
-    
-    df.rf <- df.rf[c(2,14),]
-    row.names(df.rf) <- c('AUC', 'F1')
-    df.rf <- t(df.rf)
-    
-    df.logreg <- df.logreg[c(2,14),]
-    row.names(df.logreg) <- c('AUC', 'F1')
-    df.logreg <- t(df.logreg)
-    
-    df <- data.frame(rbind(df.svm, df.rf, df.logreg))
-    df <- rownames_to_column(df, var = 'algorithm')
-    df <- separate(df,algorithm,c('algorithm', 'times'))
-    
-    
-    df$group <- x
-    df.com <- rbind(df.com, df)
-  }
+
+columns = c('algorithm', 'times', 'AUC', 'F1', 'group') 
+df.com = data.frame(matrix(nrow = 0, ncol = length(columns))) 
+colnames(df.com) = columns
+
+for (i in 1:length(sub.dirs)) {
+  sub.dir <- sub.dirs[i]
+  
+  df.svm <- read.delim(paste0(dir, '/', sub.dir, '/', paste(sub.dir, alg1, sep = '_'), '_sum.txt'))
+  df.rf <- read.delim(paste0(dir, '/', sub.dir, '/', paste(sub.dir, alg2, sep = '_'), '_sum.txt'))
+  df.logreg <- read.delim(paste0(dir, '/', sub.dir, '/', paste(sub.dir, alg3, sep = '_'), '_sum.txt'))
+  
+  df.svm <- df.svm[c(2,14),]# AUC-ROC and F1
+  row.names(df.svm) <- c('AUC', 'F1')
+  df.svm <- t(df.svm)
+  
+  df.rf <- df.rf[c(2,14),]
+  row.names(df.rf) <- c('AUC', 'F1')
+  df.rf <- t(df.rf)
+  
+  df.logreg <- df.logreg[c(2,14),]
+  row.names(df.logreg) <- c('AUC', 'F1')
+  df.logreg <- t(df.logreg)
+  
+  df <- data.frame(rbind(df.svm, df.rf, df.logreg))
+  df <- rownames_to_column(df, var = 'algorithm')
+  df <- separate(df,algorithm,c('algorithm', 'times'))
+  
+  
+  df$group <- sub.dirs[i]
+  df.com <- rbind(df.com, df)
   
 }
 
 
-# F1 plot
-p <- ggplot(data = df.com, 
-            aes(x = group,
-                y = as.numeric(F1),
-                fill = as.factor(algorithm))) +
-  geom_boxplot() +
-  ylim(0.5, 1.0) +
-  geom_jitter(shape=16, position=position_dodge(1)) +
-  geom_hline(yintercept = 0.5, linetype = 'dashed') +
-  scale_fill_discrete(name = "Algorithm") +
-  scale_fill_brewer(palette="RdBu") +
-  labs(title = expression(bolditalic('pCRE ML results')), y = 'F1', fill = 'Algorithm') + # change y axis F1/AUC
-  coord_flip() +
-  theme_minimal() +
-  theme(axis.text.y = element_text(size = 20),
-        axis.title.y = element_blank(),
-        panel.grid.minor = element_blank())
+df.com$group <- factor(df.com$group,
+                       levels = c('UU', 'ND', 'NU', 'DD'))
 
-pdf(paste0(dir, 'F1.pdf'),
-    width = 4, height = 4)
-p
-dev.off()
-
-
-# AUC_ROC plot
+# AUC_ROC
 p <- ggplot(data = df.com, 
             aes(x = group,
                 y = as.numeric(AUC),
@@ -141,15 +103,44 @@ p <- ggplot(data = df.com,
   geom_hline(yintercept = 0.5, linetype = 'dashed') +
   scale_fill_discrete(name = "Algorithm") +
   scale_fill_brewer(palette="RdBu") +
-  labs(title = expression(bolditalic('pCRE ML results')), y = 'AUC-ROC', fill = 'Algorithm') + # change y axis F1/AUC
+  labs(title = expression(bolditalic('ML resutls of pCRE')),
+       x = '',
+       y = 'AUC_ROC',
+       fill = 'Algorithm') + # change y axis F1/AUC
   coord_flip() +
   theme_minimal() +
-  theme(axis.text.y = element_text(size = 20),
-        axis.title.y = element_blank(),
-        panel.grid.minor = element_blank())
+  theme(axis.text.x = element_text(size = 10),
+        legend.position = 'bottom',
+        legend.box = 'horizontal')
 
-pdf(paste0(dir, 'AUC_ROC.pdf'),
-    width = 4, height = 4)
+tiff(paste0(dir, '/', 'AUC_ROC.tif'),
+     width = 1500, height = 1500, res = 300)
 p
 dev.off()
 
+
+# F1
+p <- ggplot(data = df.com, 
+            aes(x = group,
+                y = as.numeric(F1),
+                fill = as.factor(algorithm))) +
+  geom_boxplot() +
+  ylim(0.5, 1.0) +
+  geom_jitter(shape=16, position=position_dodge(1)) +
+  geom_hline(yintercept = 0.5, linetype = 'dashed') +
+  scale_fill_discrete(name = "Algorithm") +
+  scale_fill_brewer(palette="RdBu") +
+  labs(title = expression(bolditalic('ML resutls of pCRE')),
+       x = '',
+       y = 'F1',
+       fill = 'Algorithm') + # change y axis F1/AUC
+  coord_flip() +
+  theme_minimal() +
+  theme(axis.text.x = element_text(size = 10),
+        legend.position = 'bottom',
+        legend.box = 'horizontal')
+
+tiff(paste0(dir, '/', 'F1.tif'),
+     width = 1500, height = 1500, res = 300)
+p
+dev.off()
